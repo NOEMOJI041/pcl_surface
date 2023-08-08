0f6a9c5500586531a562c2fc68b57182a3156155
NOEMOJI041 <aloksharma041@gmail.com>
2023-08-08 22:03:55 +0530

    marker normal added

--- src/surface_detection.py
+++ src/surface_detection.py
@@ -16,12 +16,12 @@ if __name__ == "__main__":
     print("Current working directory:", os.getcwd())
 
     # Load the .ply file
-    file_path = "/home/aakshar/Desktop/point_cloud/output.ply"
+    file_path = "/home/noemoji041/Desktop/point_cloud/output.ply"
     try:
         pcd_load = o3d.io.read_point_cloud(file_path)
 #        pcd_load.compute_point_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1,max_nn=3000))
 #        pcd_load.orient_normals_to_align_with_direction()
-        pcd_load.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1,max_nn=30))
+        pcd_load.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=2,max_nn=30))
     except Exception as e:
         print("Error:", e)
 
